7df7592a68e39d8b96f867241bdbe6ddff779d1e
linetachieng2108 <linarchie101@gmail.com>
2025-02-18 10:11:52 +0300

    Added components folder and header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  // template shows static data
+  template: `
+    <p>
+      header works!
+    </p>
+  `,
+  styles: ``
+})
+
+// Dynamic data is displayed here
+export class HeaderComponent {
+
+}
